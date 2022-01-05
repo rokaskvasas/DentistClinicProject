ad466b24e8efd6a34c624c95695ac0de2a168af0
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-05 21:19:10 +0200

    Created Get and post methods

--- src/main/java/eu/codeacademy/projecttooth/tooth/controller/AppointmentController.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/controller/AppointmentController.java
@@ -1,9 +1,16 @@
 package eu.codeacademy.projecttooth.tooth.controller;
 
+import eu.codeacademy.projecttooth.tooth.dto.AppointmentDto;
+import eu.codeacademy.projecttooth.tooth.dto.DoctorServiceAvailabilityDto;
+import eu.codeacademy.projecttooth.tooth.model.Appointment;
+import eu.codeacademy.projecttooth.tooth.security.UserPrincipal;
+import eu.codeacademy.projecttooth.tooth.service.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Secured({"ROLE_PATIENT", "ROLE_DOCTOR", "ROLE_ADMIN"})
 @RestController
@@ -11,10 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AppointmentController {
 
-//    private final AppointmentEntityService service;
-//
-//    @PostMapping
-//    public void createAppointment(@AuthenticationPrincipal UserPrincipal principal, @RequestBody DoctorAvailabilityService doctorAvailabilityService) {
-//        service.createAppointment(principal, doctorAvailabilityService);
-//    }
+    private final AppointmentService service;
+
+    @GetMapping
+    public List<AppointmentDto> getAllAppointments(@AuthenticationPrincipal UserPrincipal principal){
+        return service.getAppointmentList(principal.getUserId());
+    }
+
+    @PostMapping
+    public void createAppointment(@AuthenticationPrincipal UserPrincipal principal,@RequestBody DoctorServiceAvailabilityDto payload){
+        service.createAppointment(principal.getUserId(), payload);
+    }
 }

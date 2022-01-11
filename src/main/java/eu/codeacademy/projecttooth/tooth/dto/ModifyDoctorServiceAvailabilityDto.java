9129080a8ba33c7a9cee12d96c99c860073bffe3
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-11 23:26:36 +0200

    bunch of changes 4

---
+++ src/main/java/eu/codeacademy/projecttooth/tooth/dto/ModifyDoctorServiceAvailabilityDto.java
@@ -0,0 +1,17 @@
+package eu.codeacademy.projecttooth.tooth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModifyDoctorServiceAvailabilityDto {
+
+    private Long doctorAvailabilityId;
+
+    private Long serviceId;
+}

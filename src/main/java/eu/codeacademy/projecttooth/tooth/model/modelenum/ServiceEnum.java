79109a4c02c257f278d0fc295b553a5de0f732cc
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-11 19:07:10 +0200

    Added Long values to enums.

--- src/main/java/eu/codeacademy/projecttooth/tooth/model/modelenum/ServiceEnum.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/model/modelenum/ServiceEnum.java
@@ -1,9 +1,16 @@
 package eu.codeacademy.projecttooth.tooth.model.modelenum;
 
 public enum ServiceEnum {
-    FILLING,
-    ORAL_HYGIENE,
-    ROOT_CANAL_TREATMENT,
-    PROSTHETIC,
-    CONSULTATION
+    FILLING(1L),
+    ORAL_HYGIENE(2L),
+    ROOT_CANAL_TREATMENT(3L),
+    PROSTHETIC(4L),
+    CONSULTATION(5L);
+
+    private final Long service;
+
+    ServiceEnum(Long service) {
+        this.service = service;
+    }
+    public Long getService(){return service;}
 }

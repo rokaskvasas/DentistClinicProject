3094dd4f74defe8718c4772f00f6d926437b2725
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 21:12:02 +0200

    Created scheduler configuration

---
+++ src/main/java/eu/codeacademy/projecttooth/tooth/scheduler/SchedulingConfiguration.java
@@ -0,0 +1,11 @@
+package eu.codeacademy.projecttooth.tooth.scheduler;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enable", havingValue = "true")
+public class SchedulingConfiguration {
+}

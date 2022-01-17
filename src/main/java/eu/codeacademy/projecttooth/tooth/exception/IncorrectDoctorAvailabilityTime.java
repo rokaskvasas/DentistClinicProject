3b75632ba6dab4d0f1d9fbd02d185ddc914b7552
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 21:13:14 +0200

    Changed exception name

--- src/main/java/eu/codeacademy/projecttooth/tooth/exception/IncorrectDoctorAvailabilityTime.java
+++
@@ -1,7 +0,0 @@
-package eu.codeacademy.projecttooth.tooth.exception;
-
-public class IncorrectDoctorAvailabilityTime extends RuntimeException {
-    public IncorrectDoctorAvailabilityTime(String msg) {
-        super(msg);
-    }
-}

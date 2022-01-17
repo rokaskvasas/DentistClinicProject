080d040a0e856309bf85e211e13ea503495123e2
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 16:36:45 +0200

    Added getUserByEmail method

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/impl/UserServiceImpl.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package eu.codeacademy.projecttooth.tooth.service.impl;
 
+import eu.codeacademy.projecttooth.tooth.dto.UserDto;
 import eu.codeacademy.projecttooth.tooth.exception.ObjectNotFoundException;
 import eu.codeacademy.projecttooth.tooth.mapper.UserMapper;
 import eu.codeacademy.projecttooth.tooth.model.User;
@@ -11,5 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    @Override
+    public UserDto getUserByEmail(String email){
+        return mapper.createDtoModel(repository.findByEmail(email).orElseThrow(() -> new ObjectNotFoundException(String.format("User by email: %s not found", email))));
+    }
 
 }

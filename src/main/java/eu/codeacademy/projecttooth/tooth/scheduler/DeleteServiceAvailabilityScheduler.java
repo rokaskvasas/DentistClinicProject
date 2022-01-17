4159a3654c6d10a11e7e0263ff1367d48c2c3adc
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 21:21:58 +0200

    Created delete method for expired DoctorAvailability

--- src/main/java/eu/codeacademy/projecttooth/tooth/scheduler/DeleteServiceAvailabilityScheduler.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/scheduler/DeleteServiceAvailabilityScheduler.java
@@ -13,7 +13,7 @@ public class DeleteServiceAvailabilityScheduler {
 
     // cron setting = top of every hour, on the hour 8-to-21 weekdays
     @Scheduled(cron = "0 0 8-21 * * MON-FRI")
-    public void deleteDoctorServiceAvailabilityPastTime(){
+    public void deleteDoctorServiceAvailabilityPastTime() {
         service.deleteExpiredServiceAvailability();
     }
 }

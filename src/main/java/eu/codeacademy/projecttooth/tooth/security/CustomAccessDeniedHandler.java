474d200bde4a3b9160b1eab2f55a7dde9424524f
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 14:41:20 +0200

    Created custom access denied handler

---
+++ src/main/java/eu/codeacademy/projecttooth/tooth/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package eu.codeacademy.projecttooth.tooth.security;
+
+import eu.codeacademy.projecttooth.tooth.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        ErrorResponse error = new ErrorResponse("Message: " + accessDeniedException.getMessage());
+
+        try {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(error.getMessage());
+        } catch (Exception e) {
+            log.error("Exception in AccessDeniedHandler");
+            e.getMessage();
+        }
+    }
+}

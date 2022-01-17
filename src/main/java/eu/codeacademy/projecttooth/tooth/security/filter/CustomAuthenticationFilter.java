3b587f936c09ad2d786d73f09dff5e4567bf10e6
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 19:11:18 +0200

    Added Header setter

--- src/main/java/eu/codeacademy/projecttooth/tooth/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/security/filter/CustomAuthenticationFilter.java
@@ -65,7 +65,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
         tokens.put("refresh_token", refresh_token);
-//        response.setHeader("access_token", access_token);
+        response.setHeader("AUTHORIZATION", access_token);
 //        response.setHeader("refresh_token", refresh_token);
 
 

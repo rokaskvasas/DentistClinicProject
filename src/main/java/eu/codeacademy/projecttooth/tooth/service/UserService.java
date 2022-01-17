21a909cfe29bcb70e367599bce15943560c91169
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 16:36:54 +0200

    Added getUserByEmail method

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
@@ -1,9 +1,12 @@
 package eu.codeacademy.projecttooth.tooth.service;
 
 
+import eu.codeacademy.projecttooth.tooth.dto.UserDto;
 import eu.codeacademy.projecttooth.tooth.model.User;
 import org.springframework.stereotype.Service;
 
 
 public interface UserService {
+
+    UserDto getUserByEmail(String email);
 }

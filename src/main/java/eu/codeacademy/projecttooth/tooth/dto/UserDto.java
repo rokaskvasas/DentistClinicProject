55e34e307af348d3102a0889137526797ede0ce3
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-17 16:37:58 +0200

    Added dependencies to model

--- src/main/java/eu/codeacademy/projecttooth/tooth/dto/UserDto.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/dto/UserDto.java
@@ -1,4 +1,25 @@
 package eu.codeacademy.projecttooth.tooth.dto;
 
-public abstract class UserDto {
+import lombok.*;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String password;
+
+    private String role;
 }

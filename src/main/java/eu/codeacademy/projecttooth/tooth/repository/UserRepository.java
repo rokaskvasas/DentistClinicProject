e525f50a38978206c6b23c2f19bf8b7c973f4795
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-27 17:23:09 +0200

    Added extend JpaSpecification

--- src/main/java/eu/codeacademy/projecttooth/tooth/repository/UserRepository.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/repository/UserRepository.java
@@ -2,12 +2,13 @@ package eu.codeacademy.projecttooth.tooth.repository;
 
 import eu.codeacademy.projecttooth.tooth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
 
     Optional<UserEntity> findByEmail(String email);
 }

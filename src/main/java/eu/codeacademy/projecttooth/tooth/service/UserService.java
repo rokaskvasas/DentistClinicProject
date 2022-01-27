e8395d2bede1f456ea94454c4f652ff67b769317
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-27 17:23:33 +0200

    Added delete method for unverified users

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
@@ -2,11 +2,11 @@ package eu.codeacademy.projecttooth.tooth.service;
 
 
 import eu.codeacademy.projecttooth.tooth.dto.UserDto;
-import eu.codeacademy.projecttooth.tooth.model.User;
-import org.springframework.stereotype.Service;
 
 
 public interface UserService {
 
     UserDto getUserByEmail(String email);
+
+    void deleteUnverifiedUsers();
 }
--- src/main/java/eu/codeacademy/projecttooth/tooth/service/impl/UserServiceImpl.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package eu.codeacademy.projecttooth.tooth.service.impl;
 
 import eu.codeacademy.projecttooth.tooth.dto.UserDto;
+import eu.codeacademy.projecttooth.tooth.entity.UserEntity;
 import eu.codeacademy.projecttooth.tooth.exception.ObjectNotFoundException;
 import eu.codeacademy.projecttooth.tooth.mapper.UserMapper;
-import eu.codeacademy.projecttooth.tooth.model.User;
 import eu.codeacademy.projecttooth.tooth.repository.UserRepository;
 import eu.codeacademy.projecttooth.tooth.service.UserService;
+import eu.codeacademy.projecttooth.tooth.specification.UserSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,22 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final UserSpecification userSpecification;
+
 
     @Override
-    public UserDto getUserByEmail(String email){
+    public UserDto getUserByEmail(String email) {
         return mapper.createDtoModel(repository.findByEmail(email).orElseThrow(() -> new ObjectNotFoundException(String.format("User by email: %s not found", email))));
+    }
+
+    @Override
+    public void deleteUnverifiedUsers() {
+
+        repository.deleteAllById(unverifiedUsersId());
+    }
+
+    private Iterable<Long> unverifiedUsersId() {
+        return () -> repository.findAll(userSpecification.findUnverifiedUsers()).stream().mapToLong(UserEntity::getUserId).iterator();
     }
 
 }

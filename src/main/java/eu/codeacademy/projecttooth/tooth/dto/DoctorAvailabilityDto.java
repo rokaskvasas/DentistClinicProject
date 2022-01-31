774d79469271a2b25988286f73bf38f057623109
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-31 23:01:55 +0200

    Added validation

--- src/main/java/eu/codeacademy/projecttooth/tooth/dto/DoctorAvailabilityDto.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/dto/DoctorAvailabilityDto.java
@@ -5,17 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ModifyDoctorAvailabilityDto {
+public class DoctorAvailabilityDto {
 
+    @NotNull
     private Long doctorAvailabilityId;
-
+    @NotNull
     private LocalDateTime startTime;
-
+    @NotNull
     private LocalDateTime endTime;
 }

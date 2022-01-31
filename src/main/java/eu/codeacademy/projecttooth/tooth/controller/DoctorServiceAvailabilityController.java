fe3fc00d5e7a6dcf3f856af6617f2e102214522d
rokaskvasas <rokas.kvasas@gmail.com>
2022-01-31 22:59:35 +0200

    Changed one method

--- src/main/java/eu/codeacademy/projecttooth/tooth/controller/DoctorServiceAvailabilityController.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/controller/DoctorServiceAvailabilityController.java
@@ -2,22 +2,18 @@ package eu.codeacademy.projecttooth.tooth.controller;
 
 import eu.codeacademy.projecttooth.tooth.dto.DoctorServiceAvailabilityResponse;
 import eu.codeacademy.projecttooth.tooth.dto.ModifyDoctorServiceAvailabilityDto;
+import eu.codeacademy.projecttooth.tooth.helper.DoctorServiceAvailabilityPageHelper;
 import eu.codeacademy.projecttooth.tooth.model.DoctorServiceAvailability;
-import eu.codeacademy.projecttooth.tooth.model.modelenum.ServiceEnum;
+import eu.codeacademy.projecttooth.tooth.model.DoctorServiceAvailabilitySearchCriteria;
 import eu.codeacademy.projecttooth.tooth.security.UserPrincipal;
 import eu.codeacademy.projecttooth.tooth.service.DoctorServiceAvailabilityService;
-import eu.codeacademy.projecttooth.tooth.test.DSAPage;
-import eu.codeacademy.projecttooth.tooth.test.DSASearchCriteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 @Secured({"ROLE_DOCTOR", "ROLE_ADMIN"})
 @RestController
@@ -30,10 +26,8 @@ public class DoctorServiceAvailabilityController {
 
     @PreAuthorize("hasAnyRole('ROLE_PATIENT','ROLE_ADMIN')")
     @GetMapping("/available")
-    public Page<DoctorServiceAvailability> getAllDoctorAvailabilityServiceListAsPatientAndAdmin(@AuthenticationPrincipal UserPrincipal principal,
-                                                                                                @RequestParam(name = "page", required = false, defaultValue = "0") int pageNumber,
-                                                                                                @RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize) {
-        return doctorServiceAvailabilityService.getAvailabilityServiceAsPatientAndAdmin(principal, pageNumber, pageSize);
+    public Page<DoctorServiceAvailability> getAllDoctorServiceAvailability(DoctorServiceAvailabilitySearchCriteria doctorServiceAvailabilitySearchCriteria, DoctorServiceAvailabilityPageHelper doctorServiceAvailabilityPageHelper) {
+        return doctorServiceAvailabilityService.findAvailableDoctorServiceAvailablities(doctorServiceAvailabilitySearchCriteria, doctorServiceAvailabilityPageHelper);
     }
 
     @GetMapping
@@ -66,26 +60,5 @@ public class DoctorServiceAvailabilityController {
     public Long deleteDoctorAvailabilityService(@AuthenticationPrincipal UserPrincipal principal, @PathVariable(name = "id") Long serviceId) {
         return doctorServiceAvailabilityService.deleteAvailabilityService(serviceId, principal);
     }
-
-    // problema su start ir endTime
-    @GetMapping("/test")
-    public Page<DoctorServiceAvailability> testing(DSAPage dsaPage, DSASearchCriteria dsaSearchCriteria) {
-        return doctorServiceAvailabilityService.testWithSearch(dsaPage, dsaSearchCriteria);
-    }
-
-    @GetMapping("/test2")
-    public Page<DoctorServiceAvailability> testing1(@RequestParam(name = "service", required = false) ServiceEnum serviceEnum,
-                                                    @RequestParam(name = "city", required = false) String city,
-                                                    @RequestParam(name = "startTime", required = false)
-                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
-                                                    @RequestParam(name = "endTime", required = false)
-                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime,
-                                                    @RequestParam(name = "firstName", required = false) String firstName,
-                                                    @RequestParam(name = "lastName", required = false) String lastName,
-                                                    DSAPage dsaPage) {
-
-        return doctorServiceAvailabilityService.findAvailableDoctorServiceAvailablities(city, serviceEnum, startTime, endTime, firstName, lastName, dsaPage);
-    }
-
 
 }

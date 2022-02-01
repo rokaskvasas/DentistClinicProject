976938ad84f6b0b759a19cfad4a5fafedda07f6f
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-01 18:21:01 +0200

    Deleted

--- src/main/java/eu/codeacademy/projecttooth/tooth/exception/IncorrectDoctorForAppointmentException.java
+++
@@ -1,7 +0,0 @@
-package eu.codeacademy.projecttooth.tooth.exception;
-
-public class IncorrectDoctorForAppointmentException extends RuntimeException {
-    public IncorrectDoctorForAppointmentException(String msg) {
-        super(msg);
-    }
-}

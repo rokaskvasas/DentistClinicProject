eaf4bb3d25e0a7913193b879ed67b7fd5e1e5373
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-01 18:20:29 +0200

    Added @Secured annotation

--- src/main/java/eu/codeacademy/projecttooth/tooth/controller/ServiceController.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/controller/ServiceController.java
@@ -3,6 +3,7 @@ package eu.codeacademy.projecttooth.tooth.controller;
 import eu.codeacademy.projecttooth.tooth.model.Service;
 import eu.codeacademy.projecttooth.tooth.service.ServiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("service")
 @RequiredArgsConstructor
+@Secured({"ROLE_DOCTOR", "ROLE_ADMIN"})
 public class ServiceController {
 
     private final ServiceService serviceService;

055579bdb974a3970520b498f720aaf40ab2f7ed
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 21:50:41 +0200

    Combined few methods to one

--- src/main/java/eu/codeacademy/projecttooth/tooth/specification/AppointmentSpecification.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/specification/AppointmentSpecification.java
@@ -5,8 +5,6 @@ import eu.codeacademy.projecttooth.tooth.model.AppointmentSearchCriteria;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
@@ -16,63 +14,56 @@ import java.util.Objects;
 @Component
 public class AppointmentSpecification {
 
-    private final CriteriaBuilder criteriaBuilder;
-
-    public AppointmentSpecification(EntityManager entityManager) {
-        this.criteriaBuilder = entityManager.getCriteriaBuilder();
-    }
-
 
     private Specification<AppointmentEntity> filter(AppointmentSearchCriteria searchCriteria) {
-        return (root, query, criteriaBuilder1) -> {
+        return (root, query, criteriaBuilder) -> {
 
             List<Predicate> predicates = new ArrayList<>();
-            Join<AppointmentEntity, DoctorServiceAvailabilityEntity> doctorServiceAvailabilityEntityJoin = root.join("doctorServiceAvailability");
-            Join<DoctorServiceAvailabilityEntity, ServiceEntity> serviceEntityJoin = doctorServiceAvailabilityEntityJoin.join("service");
-            Join<DoctorServiceAvailabilityEntity, DoctorAvailabilityEntity> doctorAvailabilityEntityJoin = doctorServiceAvailabilityEntityJoin.join("doctorAvailability");
-            Join<DoctorAvailabilityEntity, DoctorEntity> doctorEntityJoin = doctorAvailabilityEntityJoin.join("doctorEntity");
-            Join<DoctorEntity, UserEntity> docUserEntityJoin = doctorEntityJoin.join("user");
-            Join<DoctorEntity, LocationEntity> locationEntityJoin = doctorEntityJoin.join("location");
+            Join<AppointmentEntity, DoctorServiceAvailabilityEntity> doctorServiceAvailabilityEntityJoin = root.join(AppointmentEntity_.doctorServiceAvailability);
+            Join<DoctorServiceAvailabilityEntity, ServiceEntity> serviceEntityJoin = doctorServiceAvailabilityEntityJoin.join(DoctorServiceAvailabilityEntity_.service);
+            Join<DoctorServiceAvailabilityEntity, DoctorAvailabilityEntity> doctorAvailabilityEntityJoin = doctorServiceAvailabilityEntityJoin.join(DoctorServiceAvailabilityEntity_.doctorAvailability);
+            Join<DoctorAvailabilityEntity, DoctorEntity> doctorEntityJoin = doctorAvailabilityEntityJoin.join(DoctorAvailabilityEntity_.doctorEntity);
+            Join<DoctorEntity, UserEntity> docUserEntityJoin = doctorEntityJoin.join(DoctorEntity_.user);
+            Join<DoctorEntity, LocationEntity> locationEntityJoin = doctorEntityJoin.join(DoctorEntity_.location);
 
             if (Objects.nonNull(searchCriteria.getService())) {
-                predicates.add(criteriaBuilder.equal(serviceEntityJoin.get("name"), searchCriteria.getService()));
+                predicates.add(criteriaBuilder.equal(serviceEntityJoin.get(ServiceEntity_.name), searchCriteria.getService()));
             }
             if (Objects.nonNull(searchCriteria.getCity())) {
-                predicates.add(criteriaBuilder.like(locationEntityJoin.get("city"), "%" + searchCriteria.getCity() + "%"));
+                predicates.add(criteriaBuilder.like(locationEntityJoin.get(LocationEntity_.city), "%" + searchCriteria.getCity() + "%"));
             }
             if (Objects.nonNull(searchCriteria.getStartTime())) {
-                predicates.add(criteriaBuilder.greaterThan(root.get("startTime"), searchCriteria.getStartTime()));
+                predicates.add(criteriaBuilder.greaterThan(root.get(AppointmentEntity_.startTime), searchCriteria.getStartTime()));
             }
             if (Objects.nonNull(searchCriteria.getEndTime())) {
-                predicates.add(criteriaBuilder.lessThan(root.get("endTime"), searchCriteria.getEndTime()));
+                predicates.add(criteriaBuilder.lessThan(root.get(AppointmentEntity_.endTime), searchCriteria.getEndTime()));
             }
-            if(Objects.nonNull(searchCriteria.getFirstName())){
-                predicates.add(criteriaBuilder.like(docUserEntityJoin.get("firstName"),searchCriteria.getFirstName()));
+            if (Objects.nonNull(searchCriteria.getFirstName())) {
+                predicates.add(criteriaBuilder.like(docUserEntityJoin.get(UserEntity_.firstName), "%" + searchCriteria.getFirstName() + "%"));
             }
-            if(Objects.nonNull(searchCriteria.getLastName())){
-                predicates.add(criteriaBuilder.like(docUserEntityJoin.get("lastName"),searchCriteria.getLastName()));
+            if (Objects.nonNull(searchCriteria.getLastName())) {
+                predicates.add(criteriaBuilder.like(docUserEntityJoin.get(UserEntity_.lastName), "%" + searchCriteria.getLastName() + "%"));
             }
-
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 
-    private Specification<AppointmentEntity> setFilterForPatient(Long userId){
-        return (root, query, criteriaBuilder1) -> {
-            Join<AppointmentEntity,PatientEntity> patientEntityJoin = root.join("patient");
-            Join<PatientEntity, UserEntity> userEntityJoin = patientEntityJoin.join("user");
-            return criteriaBuilder.equal(userEntityJoin.get("userId"),userId);
+    private Specification<AppointmentEntity> setFilterForPatient(Long userId) {
+        return (root, query, criteriaBuilder) -> {
+            Join<AppointmentEntity, PatientEntity> patientEntityJoin = root.join(AppointmentEntity_.patient);
+            Join<PatientEntity, UserEntity> userEntityJoin = patientEntityJoin.join(PatientEntity_.user);
+            return criteriaBuilder.equal(userEntityJoin.get(UserEntity_.userId), userId);
         };
     }
 
-    private Specification<AppointmentEntity> setFilterForDoctor(Long userId){
-        return (root, query, criteriaBuilder1) -> {
-            Join<AppointmentEntity, DoctorServiceAvailabilityEntity> doctorServiceAvailabilityEntityJoin = root.join("doctorServiceAvailability");
-            Join<DoctorServiceAvailabilityEntity, DoctorAvailabilityEntity> doctorAvailabilityEntityJoin = doctorServiceAvailabilityEntityJoin.join("doctorAvailability");
-            Join<DoctorAvailabilityEntity, DoctorEntity> doctorEntityJoin = doctorAvailabilityEntityJoin.join("doctorEntity");
-            Join<DoctorEntity, UserEntity> docUserEntityJoin = doctorEntityJoin.join("user");
-            return criteriaBuilder.equal(docUserEntityJoin.get("userId"),userId);
+    private Specification<AppointmentEntity> setFilterForDoctor(Long userId) {
+        return (root, query, criteriaBuilder) -> {
+            Join<AppointmentEntity, DoctorServiceAvailabilityEntity> doctorServiceAvailabilityEntityJoin = root.join(AppointmentEntity_.doctorServiceAvailability);
+            Join<DoctorServiceAvailabilityEntity, DoctorAvailabilityEntity> doctorAvailabilityEntityJoin = doctorServiceAvailabilityEntityJoin.join(DoctorServiceAvailabilityEntity_.doctorAvailability);
+            Join<DoctorAvailabilityEntity, DoctorEntity> doctorEntityJoin = doctorAvailabilityEntityJoin.join(DoctorAvailabilityEntity_.doctorEntity);
+            Join<DoctorEntity, UserEntity> docUserEntityJoin = doctorEntityJoin.join(DoctorEntity_.user);
+            return criteriaBuilder.equal(docUserEntityJoin.get(UserEntity_.userId), userId);
         };
     }
 
@@ -81,7 +72,7 @@ public class AppointmentSpecification {
         return Specification.where(filter(searchCriteria)).and(setFilterForPatient(userId));
     }
 
-    public Specification<AppointmentEntity> findAllWithFiltersForDoctor(AppointmentSearchCriteria searchCriteria, Long userId){
+    public Specification<AppointmentEntity> findAllWithFiltersForDoctor(AppointmentSearchCriteria searchCriteria, Long userId) {
         return Specification.where(filter(searchCriteria)).and(setFilterForDoctor(userId));
     }
 }

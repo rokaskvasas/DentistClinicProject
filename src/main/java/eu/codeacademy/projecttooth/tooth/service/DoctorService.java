20f40e5471d5668a17760faf3c5a5af817cc843e
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 21:56:31 +0200

    Added few methods for unverified users

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/DoctorService.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/DoctorService.java
@@ -2,12 +2,18 @@ package eu.codeacademy.projecttooth.tooth.service;
 
 
 import eu.codeacademy.projecttooth.tooth.dto.DoctorDto;
+import eu.codeacademy.projecttooth.tooth.dto.DoctorRegisterDto;
+import eu.codeacademy.projecttooth.tooth.entity.DoctorEntity;
 import eu.codeacademy.projecttooth.tooth.model.Doctor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
 
 
 public interface DoctorService {
-    Doctor createDoctor(Doctor doctor);
+
+    DoctorRegisterDto createDoctor(Doctor doctor);
 
     Doctor getDoctorByUserId(Long userId);
 
@@ -18,4 +24,10 @@ public interface DoctorService {
     Page<Doctor> findAllDoctorsByStatus(String status, int pageNumber, int pageSize);
 
     Doctor verifyDoctor(Long doctorId);
+
+    List<DoctorEntity> findAllDoctorsWithSpecification(Specification<DoctorEntity> specification);
+
+    void deleteUnverifiedDoctors(Iterable<DoctorEntity> expiredDoctors);
+
+    DoctorEntity findDoctorEntity(Long userId);
 }

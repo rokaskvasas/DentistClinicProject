211862c977797f288470a8f343fc483513caac06
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 22:02:29 +0200

    Changed patient to patientRegisterDto

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/PatientService.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/PatientService.java
@@ -1,13 +1,14 @@
 package eu.codeacademy.projecttooth.tooth.service;
 
 import eu.codeacademy.projecttooth.tooth.dto.PatientDto;
+import eu.codeacademy.projecttooth.tooth.dto.PatientRegisterDto;
 import eu.codeacademy.projecttooth.tooth.entity.PatientEntity;
 import eu.codeacademy.projecttooth.tooth.model.Patient;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface PatientService {
-    Patient createPatient(Patient patient);
+    PatientRegisterDto createPatient(Patient patient);
 
     Patient getPatient(Long userId);
 

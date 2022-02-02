2e40cd5ae77f5c1d5b59df26b7c3914dd38910ef
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 22:03:29 +0200

    Added few methods

--- src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/service/UserService.java
@@ -2,11 +2,19 @@ package eu.codeacademy.projecttooth.tooth.service;
 
 
 import eu.codeacademy.projecttooth.tooth.dto.UserDto;
+import eu.codeacademy.projecttooth.tooth.entity.UserEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
 
 
 public interface UserService {
 
     UserDto getUserByEmail(String email);
 
-    void deleteUnverifiedUsers();
+    boolean findIfEmailExist(String email);
+
+    List<UserEntity> findAllUsersWithSpecification(Specification<UserEntity> specification);
+
+    void deleteUnverifiedUsers(Iterable<UserEntity> expiredUsers);
 }

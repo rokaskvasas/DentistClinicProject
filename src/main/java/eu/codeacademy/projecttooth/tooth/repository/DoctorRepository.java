0fe0f20f8b295d4dd8ee5cbada6e5b6b04fc596a
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 21:55:57 +0200

    Added JpaSpecification

--- src/main/java/eu/codeacademy/projecttooth/tooth/repository/DoctorRepository.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/repository/DoctorRepository.java
@@ -5,20 +5,20 @@ import eu.codeacademy.projecttooth.tooth.model.modelenum.StatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {
+public interface DoctorRepository extends JpaRepository<DoctorEntity, Long>, JpaSpecificationExecutor<DoctorEntity> {
 
 
     @Query("SELECT d FROM DoctorEntity as d LEFT JOIN UserEntity u ON d.user.userId = u.userId where u.userId= ?1")
     Optional<DoctorEntity> findDoctorByUserId(Long userId);
 
-    @Modifying
-    void removeByUser_UserId(Long userId);
 
     @Query("select doc from DoctorEntity doc where doc.status =?1")
     Page<DoctorEntity> findAllDoctorsByStatus(StatusEnum status, Pageable page);
+
+
 }

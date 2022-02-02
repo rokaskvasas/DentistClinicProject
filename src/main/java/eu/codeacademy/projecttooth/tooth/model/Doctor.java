8628930969ca1f9dc2aca8ba215a8c456ddf40d9
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 21:51:02 +0200

    Added validation

--- src/main/java/eu/codeacademy/projecttooth/tooth/model/Doctor.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/model/Doctor.java
@@ -6,6 +6,7 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -17,17 +18,16 @@ public class Doctor extends User {
 
     private Long doctorId;
 
-    private Long userId;
     @NotBlank
     @Size(min = 5, max = 70)
     private String doctorLicense;
 
     private StatusEnum status;
-
+    @NotNull
     private QualificationEnum qualification;
 
     private Location location;
-
+    @NotNull
     private Long locationId;
 
 

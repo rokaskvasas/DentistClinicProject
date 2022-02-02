819f35500dd5d7d82d4fb9e8149d21cb86ebbcdd
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-02 22:02:14 +0200

    Created createPatientRegisterDtoModel method

--- src/main/java/eu/codeacademy/projecttooth/tooth/mapper/PatientMapper.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/mapper/PatientMapper.java
@@ -1,5 +1,6 @@
 package eu.codeacademy.projecttooth.tooth.mapper;
 
+import eu.codeacademy.projecttooth.tooth.dto.PatientRegisterDto;
 import eu.codeacademy.projecttooth.tooth.entity.PatientEntity;
 import eu.codeacademy.projecttooth.tooth.entity.UserEntity;
 import eu.codeacademy.projecttooth.tooth.model.Patient;
@@ -14,19 +15,20 @@ public class PatientMapper {
 
     private final UserMapper userMapper;
 
-    public PatientEntity createEntity(Patient patient){
+    public PatientEntity createEntity(Patient patient) {
 
         return PatientEntity.builder()
                 .user(userMapper.getUserEntity(patient)).build();
     }
-    public Patient createModel(PatientEntity entity){
+
+    public Patient createModel(PatientEntity entity) {
         Patient.PatientBuilder<?, ?> builder = Patient.builder();
         setUserDetailsToPatient(entity.getUser(), builder);
         return builder.patientId(entity.getPatientId()).build();
     }
 
     private void setUserDetailsToPatient(UserEntity user, Patient.PatientBuilder<?, ?> builder) {
-        if(Objects.nonNull(user)){
+        if (Objects.nonNull(user)) {
             builder
                     .firstName(user.getFirstName())
                     .lastName(user.getLastName())
@@ -35,6 +37,14 @@ public class PatientMapper {
                     .userId(user.getUserId())
                     .role(user.getRole());
         }
+    }
+
+    public PatientRegisterDto createPatientRegisterDtoModel(PatientEntity entity) {
+        return PatientRegisterDto.builder()
+                .firstName(entity.getUser().getFirstName())
+                .lastName(entity.getUser().getLastName())
+                .phoneNumber(entity.getUser().getPhoneNumber())
+                .email(entity.getUser().getEmail()).build();
     }
 
 }

e0f5b93a3a379db9468d2306a3bd469df9d6c344
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-03 19:46:52 +0200

    Batch update

--- src/main/java/eu/codeacademy/projecttooth/tooth/exception/GlobalExceptionHandler.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/exception/GlobalExceptionHandler.java
@@ -19,30 +19,35 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse emailNotFound(EmailNotFoundException e) {
         log.error("Email not found", e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(IncorrectTimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse incorrectAvailabilityTime(IncorrectTimeException e) {
         log.error("Incorrect availability time", e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(QualificationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse minimumQualification(QualificationException e) {
         log.error("minimum qualification ", e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(DuplicateServiceException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse duplicateServiceException(DuplicateServiceException e) {
         log.error("duplicate service for same doctor service availability ", e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(DoctorAvailabilityReservedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse doctorAvailabilityReservedException(DoctorAvailabilityReservedException e){
         log.error("doctor availability already reserved", e);
         return new ErrorResponse(e.getMessage());

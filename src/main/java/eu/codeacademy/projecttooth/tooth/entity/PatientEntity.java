afe4800d9e205d00ad78f68df3dcb8b907b5bc98
rokaskvasas <rokas.kvasas@gmail.com>
2022-02-03 16:02:39 +0200

    Added create and update at

--- src/main/java/eu/codeacademy/projecttooth/tooth/entity/PatientEntity.java
+++ src/main/java/eu/codeacademy/projecttooth/tooth/entity/PatientEntity.java
@@ -3,6 +3,7 @@ package eu.codeacademy.projecttooth.tooth.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -25,5 +26,21 @@ public class PatientEntity {
 
     @OneToMany(mappedBy = "patient")
     private List<AppointmentEntity> appointment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void setCreatedAtDateTime() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
 }
